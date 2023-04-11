0cc7af83c60d455f7963c32043d473301931b2b5
Jones <michaeljones401@gmail.com>
2023-04-11 14:07:08 +0100

    Update snippet2a.js

--- snippet2a.js
+++ snippet2a.js
@@ -1,26 +1,26 @@
 function is_palindrome(str) {
     const len = str.length;
-    const mid = Math.floor(len / 2);
     let first = str[0];
     let last = str[len - 1];
 
     for (let i = 0; i < str.length; i++) {
-        if(i === mid) return true;
         first = str[i];
         last = str[len - 1 - i];
         if(first !== last) return false;
     }
+
+    return true;
 }
 
 // same as above but with a different variable name
 function is_palindrome_2(str) {
     const len = str.length;
-    const mid = Math.floor(len / 2);
 
     for (let i = 0; i < str.length; i++) {
-        if(i === mid) return true;
         if(str[i] !== str[len - 1 - i]) return false;
     }
+
+    return true;
 }
 
 const palindrome = "tattarrattat";
