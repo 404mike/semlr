be9ab7262140e6ac525d9af59c60261433150439
Jones <michaeljones401@gmail.com>
2023-04-11 12:46:33 +0100

    Removing old code

--- snippet2.js
+++ snippet2.js
@@ -1,10 +1,8 @@
 function is_palindrome(str) {
     let reverseStr = str.split("").reverse().join("");
     let strLength = str.length;
-    let middle = Math.round(str.length / 2);
-
+  
     for(let i = 0; i < strLength; i++) {
-        if(middle == i) return true;
         if(str[i] != reverseStr[i]) return false;
     }
 
